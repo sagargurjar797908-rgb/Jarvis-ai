4873a2a130f3645297701ff336d47c962504517f
sagargurjar797908-rgb <sagargurjar797908@gmail.com>
2026-09-04 22:05:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "online",
+    message: "JARVIS backend is online, Boss!"
+  });
+});
+
+app.post("/chat", (req, res) => {
+  const message = req.body.message || "";
+
+  res.json({
+    reply: `JARVIS received: ${message}`
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`JARVIS server running on port ${PORT}`);
+});
